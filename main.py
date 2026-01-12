cfe71bed3395d0b42da6e76ec2f460c7167d0bd8
Erick-N <rod8qy8kc@mozmail.com>
2026-01-11 23:25:59 -0500

    CH4: Asteroids, L2: Collisions - Collision detection implemented

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    newPlayer = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    newAsteroidField = AsteroidField()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -11,7 +25,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for thing in asteroids:
+            if thing.collides_with(newPlayer):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
